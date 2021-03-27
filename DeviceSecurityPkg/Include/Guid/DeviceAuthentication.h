16dafbf8b23a5030593338a4389f6ce790109f4c
Jiewen Yao <jiewen.yao@intel.com>
2021-03-28 07:42:52 +0800

    complete DevDb definition.
    Signed-off-by: Jiewen Yao <jiewen.yao@intel.com>

--- DeviceSecurityPkg/Include/Guid/DeviceAuthentication.h
+++ DeviceSecurityPkg/Include/Guid/DeviceAuthentication.h
@@ -30,14 +30,23 @@
   might not return the whole CERT chain, but the digest of the CERT chain.
   In order to verify the SPDM challenge response, the SPDM cert chain must be stored.
 **/
-#define SPDM_CERT_CHAIN_GUID \
+#define X509_CERT_CHAIN_GUID \
   {0xa4ba9c9e, 0x1574, 0x4a2e, 0x87, 0xc1, 0xc9, 0xbb, 0x87, 0x87, 0x66, 0xe6}
 
-#define SPDM_CERT_CHAIN_DIGEST_GUID \
+#define X509_CERT_CHAIN_SHA256_GUID \
   {0x14749a83, 0x5c7, 0x4f79, 0xab, 0xaf, 0x75, 0x92, 0x71, 0x73, 0xf7, 0x43}
 
+#define X509_CERT_CHAIN_SHA384_GUID \
+  {0x3c85befc, 0xf863, 0x4db2, 0x8d, 0x6a, 0xaf, 0x73, 0x25, 0x4, 0x93, 0x9a}
+
+#define X509_CERT_CHAIN_SHA512_GUID \
+  {0x4795db26, 0x2a19, 0x4ae9, 0xae, 0x98, 0x4f, 0xdf, 0xfa, 0x89, 0xe8, 0xab}
+
 extern EFI_GUID gEdkiiDeviceSignatureDatabaseGuid;
-extern EFI_GUID gEdkiiCertSpdmCertChainGuid;
+extern EFI_GUID gEdkiiCertX509CertChainGuid;
+extern EFI_GUID gEdkiiCertX509CertChainSha256Guid;
+extern EFI_GUID gEdkiiCertX509CertChainSha384Guid;
+extern EFI_GUID gEdkiiCertX509CertChainSha512Guid;
 
 /**
   Signature Database:
@@ -73,8 +82,9 @@ extern EFI_GUID gEdkiiCertSpdmCertChainGuid;
                    EFI_CERT_RSA2048_SHAxxx_GUID |
                    EFI_CERT_X509_GUID |
                    EFI_CERT_X509_SHAxxx_GUID |
-                   EFI_CERT_SPDM_CERT_CHAIN_GUID
-  (xxx = 1, 224, 256, 384, 512)
+                   EFI_CERT_X509_CERT_CHAIN_GUID |
+                   EFI_CERT_X509_CERT_CHAIN_SHAxxx_GUID
+  (xxx = 256, 384, 512)
 
 **/
 
